0ab5da4f6a4a384361166f13c1f9a34973164ffa
kicnft <143278361+kicnft@users.noreply.github.com>
2024-04-14 00:16:10 +0900

    update js

--- symapp.js
+++ symapp.js
@@ -3,26 +3,50 @@ let onSocketResumed;
 let onSocketMessage;
 let onPhysicalMessage;
 let onStatusChange;
-let nodeFetchTimout = 10000;
+let onVisible;
+let nodeFetchTimout;
+let selectedNode;
+let currentWebsocket;
 
-async function startSymbolApplication(config,ready,cb1,cb2,cb3,cb4){
-
-	nodeFetchTimout = config.timeout;
+async function startSymbolApplication(config,onReady,cb1,cb2,cb3,cb4,cb5){
 
 	//コールバック関数登録
-	onSocketMessage = cb1;
+	onStatusChange = cb1;
 	onPhysicalMessage = cb2
-	onSocketResumed = cb3;
-	onStatusChange = cb4;
+	onSocketMessage = cb3;
+	onSocketResumed = cb4;
+	onVisible = cb5;
 
-	const selectedNode = await selectNode(
+	//nodeFetchのタイムアウト設定
+	nodeFetchTimout = config.timeout;
+
+	//ノード選択
+	selectedNode = await selectNode(
 		config.nodes,
 		config.selectNodeCount,
 		config.retryCount,
 		config.heightDiffThreshold
 	);
-	ready(selectedNode);
+	onReady(selectedNode);
 }
+
+document.addEventListener("visibilitychange", async function() {
+	if (document.visibilityState === 'visible') {
+		const readyState = currentWebsocket.socket.readyState;
+		if(readyState == 1){
+			const timeoutId = setTimeout(async() => {
+				if(readyState == 3){
+					currentWebsocket = await connectWebSocket(selectedNode.url);
+					onSocketResumed(currentWebsocket);
+				}
+			}, 100);
+		}else{
+			currentWebsocket = await connectWebSocket(selectedNode.url);
+			onSocketResumed(currentWebsocket);
+		}
+		onVisible();
+	}
+});
 
 function getRandomNodes(nodes,count) {
 
@@ -36,9 +60,6 @@ function getRandomNodes(nodes,count) {
 			availableIndices.add(index);
 			randomNodes.push(nodes[index]);
 		}
-	}
-	if(randomNodes.length>3){
-		console.log(randomNodes);
 	}
 	return randomNodes;
 }
@@ -66,25 +87,24 @@ async function selectNode(nodes,selectCount = 5, retries = 5,heightDiffThreshold
 
 		// ターゲットノードと他のノードのhash値を確認する
 		const hashes = results.slice(2 +selectedNodes.length); 
-		console.log(hashes);
 		for (let i = 1; i < hashes.length; i++) {
 			if (hashes[0] !== hashes[i]) {
 				throw new Error("hashes are difference");
 			}
 		}
 
-		const ws = await connectWebSocket(targetNode);
+		currentWebsocket = await connectWebSocket(targetNode);
 		return {
 			url:targetNode,
 			network:results[1],
-			socket:ws
+			socket:currentWebsocket
 		};
 
 		// `symbolSocketUid`を使用した処理を続ける
 		// WebSocketの処理を行う
 	} catch (error) {
 		if (retries > 0) {
-			onStatusChange({name:"retry"});
+			onStatusChange({name:"retry",targetNode:targetNode,error:error.message});
 			return await selectNode(nodes, selectCount,retries - 1,heightDiffThreshold);
 		} else {
 			onStatusChange({name:"over"});
@@ -92,40 +112,57 @@ async function selectNode(nodes,selectCount = 5, retries = 5,heightDiffThreshold
 	}
 }
 
+//タイムアウト機能つきfetch
 function nodeFetch(nodeUrl, method = 'GET', data = null) {
-  const timeout = nodeFetchTimout; // タイムアウト値
 
-  const controller = new AbortController();
-  const timeoutId = setTimeout(() => {
-    controller.abort();
-  }, timeout);
+	const timeout = nodeFetchTimout; // タイムアウト値
+	const controller = new AbortController();
+	const timeoutId = setTimeout(() => {
+		controller.abort();
+	}, timeout);
 
-  const requestOptions = {
-    method,
-    headers: {
-      'Content-Type': 'application/json' // リクエストヘッダーに適切なContent-Typeを指定します
-    },
-    signal: controller.signal
-  };
+	const requestOptions = {
+		method,
+		signal: controller.signal
+	};
 
-  if (method === 'POST' || method === 'PUT') {
-    requestOptions.body = JSON.stringify(data); // リクエストボディにデータを含める場合は適切なデータ形式に変換する必要があります
-  }
+	
+/*	
+	const requestOptions = {
+		method,
+		headers: {
+			'Content-Type': 'application/json' // リクエストヘッダーに適切なContent-Typeを指定します
+		},
+		signal: controller.signal
+	};
 
-  return fetch(nodeUrl, requestOptions)
-    .then(response => {
-      clearTimeout(timeoutId);
-      if (!response.ok) {
-        throw new Error("Error: " + response.status);
-      }
-      return response.text();
-    })
-    .catch(error => {
-      if (error.name === "AbortError") {
-        throw new Error("Timeout");
-      }
-      throw error;
-    });
+
+	const requestOptions = {
+		method,
+		signal: controller.signal
+	};
+*/
+	if (method === 'POST' || method === 'PUT') {
+		requestOptions.headers = {
+			'Content-Type': 'application/json' // リクエストヘッダーに適切なContent-Typeを指定します
+		};
+		requestOptions.body = JSON.stringify(data); // リクエストボディにデータを含める場合は適切なデータ形式に変換する必要があります
+	}
+
+	return fetch(nodeUrl, requestOptions)
+	.then(response => {
+		clearTimeout(timeoutId);
+		if (!response.ok) {
+			throw new Error("Error: " + response.status);
+		}
+		return response.text();
+	})
+	.catch(error => {
+		if (error.name === "AbortError") {
+			throw new Error("Timeout");
+		}
+		throw error;
+	});
 }
 
 function checkNodeHealth(node) {
@@ -168,7 +205,9 @@ function checkLastBlockHash(node) {
 		});
 }
 
+let isSocketFailed = false;
 function connectWebSocket(targetNode) {
+
 	return new Promise((resolve, reject) => {
 		const wsEndpoint = targetNode.replace("http", "ws") + "/ws";
 		const socket = new WebSocket(wsEndpoint);
@@ -187,12 +226,18 @@ function connectWebSocket(targetNode) {
 		};
 
 		socket.onerror = function () {
+			isSocketFailed = true;
 			reject(new Error("Failed to connect to the WebSocket"));
+
 		};
 
 		socket.onclose = async function () {
-			const ws = await connectWebSocket(targetNode);
-			onSocketResumed(ws);
+			if(isSocketFailed){
+				return;
+			}
+			currentWebsocket = await connectWebSocket(targetNode);
+			isSocketFailed = false;
+			onSocketResumed(currentWebsocket);
 		};
 	});
 }
@@ -200,11 +245,6 @@ function connectWebSocket(targetNode) {
 function socketSend(ws,subscribe){
 
 	ws.socket.send(JSON.stringify({ uid:ws.uid, subscribe: subscribe }));
-}
-
-function socketSendNew(socket,uid,subscribe){
-
-	socket.send(JSON.stringify({ uid:uid, subscribe: subscribe }));
 }
 
 function dispAmount(amount,divisibility){
@@ -301,15 +341,18 @@ function getCodeInfo(src){
 		drawLine(code.location.bottomRightCorner	, code.location.bottomLeftCorner	,"#FF3B58");
 		drawLine(code.location.bottomLeftCorner		, code.location.topLeftCorner		,"#FF3B58");
 
-//		onPhysicalMessage(code.data);
-		onPhysicalMessage(JSON.parse(code.data));
+		try{
+			onPhysicalMessage(JSON.parse(code.data));
+		}catch{
+			onPhysicalMessage(code.data);
+		}
 	}
 }
 
-function startVideo(){
+function startVideo(id){
 
 	isMovieScanning = true;
-	canvasElement = document.getElementById("canvas");
+	canvasElement = document.getElementById(id);
 	canvas		  = canvasElement.getContext("2d", { willReadFrequently: true });
 
 	navigator.mediaDevices.getUserMedia({
@@ -322,9 +365,9 @@ function startVideo(){
 		window.localStream = stream;
 		video.setAttribute("playsinline", true);
 		//video.play();
-        video.onloadedmetadata = function() {
-            video.play(); // メタデータのロードが完了した後に再生を開始
-        };
+		video.onloadedmetadata = function() {
+			video.play(); // メタデータのロードが完了した後に再生を開始
+		};
 		requestAnimationFrame(tick);
 	})
 	.catch(function(err) {
@@ -386,13 +429,13 @@ function scanFileImage(tag){
 
 function createFolder(tree,id,text,parent){
 
-	const node = { id: id,  icon: 'jstree-folder', text: text, state: {'opened': true}};
+	const node = { id: id,	icon: 'jstree-folder', text: text, state: {'opened': true}};
 	$(tree).jstree(true).create_node( parent, node);
 }
 
 function createFile(tree, id,text,parent){
 
-	const node = { id: id,  icon: 'jstree-file', text: text, state: {'opened': true}};
+	const node = { id: id,	icon: 'jstree-file', text: text, state: {'opened': true}};
 	$(tree).jstree(true).create_node( parent, node);
 }
 
@@ -426,11 +469,11 @@ async function createChildNode(multisig,parent,accountNames,signer,signedCosigne
 	const address = sym.Address.createFromEncoded(multisig.accountAddress);
 	const accountName = accountNames.find(an=>an.address === multisig.accountAddress).names[0];
 //	const names = await nodeFetchAccountNames([address.plain()]);
-//		child.text = address.pretty().slice( 0, 13 ) + "..." +  address.pretty().slice( -3 );
+//		child.text = address.pretty().slice( 0, 13 ) + "..." +	address.pretty().slice( -3 );
 	if(accountName){
 		child.text = accountName;
 	}else{
-		child.text = address.pretty().slice( 0, 20 ) + "..." +  address.pretty().slice( -3 );
+		child.text = address.pretty().slice( 0, 20 ) + "..." +	address.pretty().slice( -3 );
 	}
 
 	if(multisig.accountAddress === signer){
@@ -504,9 +547,8 @@ async function getMosaicAsset(itemId){
 
 function clog(nodeUrl,signedTx){
 
-    console.log(nodeUrl + "/transactionStatus/" + signedTx.hash);
-    console.log(nodeUrl + "/transactions/confirmed/" + signedTx.hash);
-    console.log("https://symbol.fyi/transactions/" + signedTx.hash);
-    console.log("https://testnet.symbol.fyi/transactions/" + signedTx.hash);
+	console.log(nodeUrl + "/transactionStatus/" + signedTx.hash);
+	console.log(nodeUrl + "/transactions/confirmed/" + signedTx.hash);
+	console.log("https://symbol.fyi/transactions/" + signedTx.hash);
+	console.log("https://testnet.symbol.fyi/transactions/" + signedTx.hash);
 }
-
